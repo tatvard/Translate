dc4784d62b4ae9da2d0fa1229d240de198517c33
tatvard <tatevikvardanyan21@gmail.com>
2018-10-22 18:52:45 +0200

    done main screen

---
+++ app/src/main/java/com/example/test/translate/App.java
@@ -0,0 +1,17 @@
+package com.example.test.translate;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
+
+public class App extends Application {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
