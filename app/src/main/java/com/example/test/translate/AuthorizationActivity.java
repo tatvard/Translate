190ec93c1ccbc6e9ca19180d2a24a2e5caa9d21a
Tatev <tatevikvardanyan21@gmail.com>
2018-11-05 22:32:14 +0400

    done authorization screen

---
+++ app/src/main/java/com/example/test/translate/AuthorizationActivity.java
@@ -0,0 +1,20 @@
+package com.example.test.translate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class AuthorizationActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+
+    }
+
+}
