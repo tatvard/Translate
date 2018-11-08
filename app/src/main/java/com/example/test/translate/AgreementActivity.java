cb1eec6ebaeef96ec38dd06400c9c96184fd8e3f
Tatev <tatevikvardanyan21@gmail.com>
2018-11-08 21:12:43 +0400

    adding agreement screen

--- app/src/main/java/com/example/test/translate/AgreementActivity.java
+++ app/src/main/java/com/example/test/translate/AgreementActivity.java
@@ -4,32 +4,27 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class AgreementActivity extends AppCompatActivity {
-    TextView textView;
-    String content="<h1>Heading 1</h1>\n" +
-            "        <h2>Heading 2</h2>\n" +
-            "        <p>This is some html. Look, here\\'s an <u>underline</u>.</p>\n" +
-            "        <p>Look, this is <em>emphasized.</em> And here\\'s some <b>bold</b>.</p>\n" +
-            "        <p>Here are UL list items:\n" +
-            "        <ul>\n" +
-            "        <li>One</li>\n" +
-            "        <li>Two</li>\n" +
-            "        <li>Three</li>\n" +
-            "        </ul>\n" +
-            "        <p>Here are OL list items:\n" +
-            "        <ol>\n" +
-            "        <li>One</li>\n" +
-            "        <li>Two</li>\n" +
-            "        <li>Three</li>\n" +
-            "        </ol>";
+    String content="<h1>User agreement for Yandex\n" +
+            "    Services</h1>\n" +
+            "        <p><i>This document is a translation of User Agreement for\n</p>\n" +
+            "           <a href=\"https://yandex.ru/legal/ruls/?lang=ru.\" > https://yandex.ru/legal/ruls/?lang=ru.</a>\n"+
+            "         <h1>1.General Provisions\n </h1>\n"+
+            "        <p><p>1.1 Yandex LLC(hereinafter - \"Yandex\") offers\n" +
+            "    Internet users(hereinafter - the \"User\") the\n" +
+            "    opportnity of this User Agreement </p>.</p>\n";
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreement);
+        TextView textView = (TextView) findViewById(R.id.agreement);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             textView.setText(Html.fromHtml(content, Html.FROM_HTML_MODE_LEGACY));
         } else
